ddd9d879dbe1d131be8f183759db7fd0b3c58d98
Hakan Alpay <hak7alp@gmail.com>
2020-04-23 17:31:36 -0700

    added autocomplete to AddProduct.vue
    Signed-off-by: Hakan Alpay <hak7alp@gmail.com>

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,31 +1,131 @@
 <template>
-  <div class="product-action-bar">
-    <input
-      placeholder="product name..."
-      class="product-name-input"
-      type="text"
-      :value="productNameToCreate"
-      @input="setProductNameToCreate($event.target.value)"
-      @keypress.enter="triggerAddProductAction"
-    />
-    <div
-      :class="{ disabled: productCreationPending }"
-      class="create-product-btn"
-      @click="triggerAddProductAction"
-    >
-      add product
+  <v-card color="red lighten-2" dark>
+    <div class="product-action-bar">
+      <input
+        placeholder="product name..."
+        class="product-name-input"
+        type="text"
+        :value="productNameToCreate"
+        @input="setProductNameToCreate($event.target.value)"
+        @keypress.enter="triggerAddProductAction"
+      />
+      <div
+        :class="{ disabled: productCreationPending }"
+        class="create-product-btn"
+        @click="triggerAddProductAction"
+      >
+        add product
+      </div>
     </div>
-  </div>
+    <v-card-title class="headline red lighten-3"
+      >Search for a Game!</v-card-title
+    >
+    <v-card-text>
+      Explore hundreds of free API's ready for consumption! For more information
+      visit
+      <a
+        class="grey--text text--lighten-3"
+        href="https://github.com/toddmotto/public-apis"
+        target="_blank"
+        >the Github repository</a
+      >.
+    </v-card-text>
+    <v-card-text>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        item-value="API"
+        label="Public APIs"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-database-search"
+        return-object
+      ></v-autocomplete>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-expand-transition>
+      <v-list v-if="model" class="red lighten-3">
+        <v-list-item v-for="(field, i) in fields" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="field.value"></v-list-item-title>
+            <v-list-item-subtitle v-text="field.key"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-expand-transition>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn :disabled="!model" color="grey darken-3" @click="model = null">
+        Clear
+        <v-icon right>mdi-close-circle</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 import { mapMutations, mapState, mapActions } from "vuex";
 
 export default {
-  computed: mapState("products", [
-    "productNameToCreate",
-    "productCreationPending"
-  ]),
+  data: () => ({
+    descriptionLimit: 60,
+    entries: [],
+    isLoading: false,
+    model: null,
+    search: null
+  }),
+  computed: {
+    ...mapState("products", ["productNameToCreate", "productCreationPending"]),
+    fields() {
+      if (!this.model) return [];
+
+      return Object.keys(this.model).map(key => {
+        return {
+          key,
+          value: this.model[key] || "n/a"
+        };
+      });
+    },
+    items() {
+      return this.entries.map(entry => {
+        const Description =
+          entry.Description.length > this.descriptionLimit
+            ? `${entry.Description.slice(0, this.descriptionLimit)}...`
+            : entry.Description;
+
+        return { ...entry, Description };
+      });
+    }
+  },
+  watch: {
+    search() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+
+      // Items have already been requested
+      if (this.isLoading) return;
+
+      this.isLoading = true;
+
+      // Lazily load input items
+      fetch("https://api-v3.igdb.com/games/")
+        .then(res => res.json())
+        .then(res => {
+          const { count, entries } = res;
+          this.count = count;
+          this.entries = entries;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => (this.isLoading = false));
+    }
+  },
   methods: {
     ...mapMutations("products", ["setProductNameToCreate"]),
     ...mapActions("products", ["triggerAddProductAction"])
