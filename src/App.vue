b3340957e11551241245a7851c64e11672d0b17c
Hakan Alpay <hak7alp@gmail.com>
2020-04-23 17:29:21 -0700

    Added v-app wrapper to app so that vuetify would work

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <v-app>
     <nav-bar></nav-bar>
     <div class="main-wrapper">
       <router-view />
@@ -17,25 +17,25 @@
       @close="closeAddToHomeScreenModalForApple(false)"
     >
     </apple-add-to-home-screen-modal>
-  </div>
+  </v-app>
 </template>
 <script>
-import NavBar from '@/components/NavBar'
-import NewContentAvailableToastr from '@/components/NewContentAvailableToastr'
-import AppleAddToHomeScreenModal from '@/components/AppleAddToHomeScreenModal'
-import { mapState, mapActions, mapGetters } from 'vuex'
+import NavBar from "@/components/NavBar";
+import NewContentAvailableToastr from "@/components/NewContentAvailableToastr";
+import AppleAddToHomeScreenModal from "@/components/AppleAddToHomeScreenModal";
+import { mapState, mapActions, mapGetters } from "vuex";
 
 export default {
   components: { NavBar, NewContentAvailableToastr, AppleAddToHomeScreenModal },
   computed: {
-    ...mapGetters('app', ['newContentAvailable']),
-    ...mapState('app', ['showAddToHomeScreenModalForApple', 'refreshingApp'])
+    ...mapGetters("app", ["newContentAvailable"]),
+    ...mapState("app", ["showAddToHomeScreenModalForApple", "refreshingApp"])
   },
-  methods: mapActions('app', [
-    'closeAddToHomeScreenModalForApple',
-    'serviceWorkerSkipWaiting'
+  methods: mapActions("app", [
+    "closeAddToHomeScreenModalForApple",
+    "serviceWorkerSkipWaiting"
   ])
-}
+};
 </script>
 
 <style lang="scss">
