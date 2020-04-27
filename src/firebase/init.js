fff8d583f0a78ea961933774e3d7c4df3f46356f
Hakan Alpay <hak7alp@gmail.com>
2020-04-27 11:26:02 -0700

    prettier firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCpJ16nEK-_WlD5Et9SDGb-FJcxufxJ_bU',
-  authDomain: 'squad-up-21a9e.firebaseapp.com',
-  databaseURL: 'https://squad-up-21a9e.firebaseio.com',
-  projectId: 'squad-up-21a9e',
-  storageBucket: 'squad-up-21a9e.appspot.com',
-  messagingSenderId: '350232073633',
-  appId: '1:350232073633:web:af4e44f43c14e789bb67e0',
-  measurementId: 'G-K5W5NRHPTR'
-}
+  apiKey: "AIzaSyCpJ16nEK-_WlD5Et9SDGb-FJcxufxJ_bU",
+  authDomain: "squad-up-21a9e.firebaseapp.com",
+  databaseURL: "https://squad-up-21a9e.firebaseio.com",
+  projectId: "squad-up-21a9e",
+  storageBucket: "squad-up-21a9e.appspot.com",
+  messagingSenderId: "350232073633",
+  appId: "1:350232073633:web:af4e44f43c14e789bb67e0",
+  measurementId: "G-K5W5NRHPTR"
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
