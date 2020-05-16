6380d6200cf50f056617d7d336126b93b6c17600
Hakan Alpay <hak7alp@gmail.com>
2020-05-16 16:04:00 -0700

    fixed prettier problems

--- src/views/Squad.vue
+++ src/views/Squad.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="page-wrapper">
     <div class="title">
-      <h1 v-if="currentSquad" class="squad-name">Squad: {{ currentSquad.game }}</h1>
+      <h1 v-if="currentSquad" class="squad-name">
+        Squad: {{ currentSquad.game }}
+      </h1>
       <p v-if="user === null" class="infos-label">Please login to join</p>
       <div
         v-if="userCanLeave(user.displayName)"
@@ -20,9 +22,7 @@
     </div>
     <hr />
     <h2>Players</h2>
-    <h3 v-for="usr in currentSquad.users" :key="usr.id">
-      {{ usr }}
-    </h3>
+    <h3 v-for="usr in currentSquad.users" :key="usr.id">{{ usr }}</h3>
   </div>
 </template>
 
@@ -44,15 +44,6 @@ export default {
       ]
     };
   },
-  methods: {
-    ...mapActions("squads", ["joinSquad", "leaveSquad"]),
-    userCanLeave(user) {
-      return (this.getSquadById(this.$route.params.id).users.indexOf(user) > -1);
-    },
-    userCanJoin(user) {
-      return (this.getSquadById(this.$route.params.id).users.indexOf(user) === -1);
-    }
-  },
   computed: {
     ...mapState("app", ["appTitle"]),
     ...mapState("authentication", ["user"]),
@@ -63,6 +54,17 @@ export default {
       return this.getSquadById(this.$route.params.id);
     },
     ...mapGetters("squads", ["getSquadById"])
+  },
+  methods: {
+    ...mapActions("squads", ["joinSquad", "leaveSquad"]),
+    userCanLeave(user) {
+      return this.getSquadById(this.$route.params.id).users.indexOf(user) > -1;
+    },
+    userCanJoin(user) {
+      return (
+        this.getSquadById(this.$route.params.id).users.indexOf(user) === -1
+      );
+    }
   }
 };
 </script>
@@ -70,41 +72,41 @@ export default {
 <style lang="scss" scoped>
 @import "@/theme/variables.scss";
 
-  .squad-name {
-    display: inline-block;
-  }
+.squad-name {
+  display: inline-block;
+}
 
-  .infos-label {
-    display: inline-block;
-  }
+.infos-label {
+  display: inline-block;
+}
 
-  .title {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    padding-bottom: 10px;
-  }
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding-bottom: 10px;
+}
 
-  .accept-btn {
-    cursor: pointer;
-    padding: 5px 10px;
-    border: 1px solid;
-    display: inline-block;
-    border-radius: 3px;
-    margin-left: auto;
-    color: $vue-color;
-    border-color: $vue-color;
-  }
+.accept-btn {
+  cursor: pointer;
+  padding: 5px 10px;
+  border: 1px solid;
+  display: inline-block;
+  border-radius: 3px;
+  margin-left: auto;
+  color: $vue-color;
+  border-color: $vue-color;
+}
 
-  .delete-btn {
-    cursor: pointer;
-    padding: 5px 10px;
-    border: 1px solid;
-    display: inline-block;
-    border-radius: 3px;
-    margin-left: auto;
-    color: $danger-color;
-    border-color: $danger-color;
-  }
+.delete-btn {
+  cursor: pointer;
+  padding: 5px 10px;
+  border: 1px solid;
+  display: inline-block;
+  border-radius: 3px;
+  margin-left: auto;
+  color: $danger-color;
+  border-color: $danger-color;
+}
 </style>
