3b366b1759a503dac6b1a168ad82e74794daf250
rohittavare <rohit.t797@yahoo.com>
2020-05-31 17:20:46 -0700

    remove functions

--- functions/squads-db.js
+++
@@ -1,9 +0,0 @@
-import GenericDB from "./generic-db";
-
-export default class SquadsDB extends GenericDB {
-  constructor() {
-    super("squads");
-  }
-
-  // Here you can extend SquadsDB with custom methods
-}
